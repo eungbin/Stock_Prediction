454d9bcb91fd1ed9c887054def050d0983e96391
kim <programmer97@naver.com>
2020-09-07 14:56:59 +0900

    data-normalization

--- LSTM_Stock_Prediction/LSTM_model.py
+++ LSTM_Stock_Prediction/LSTM_model.py
@@ -1,13 +1,30 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
+from sklearn.preprocessing import MinMaxScaler
 
-csv_data = pd.read_csv('C:/Users/kim/Desktop/git_folder/LSTM_Stock_Prediction/csv/005930.KS.csv')
+csv_data = pd.read_csv('C:/dev/react/stock_predict/LSTM_Stock_Prediction/csv/005930.KS.csv')
 
 csv_data['Date'] = pd.to_datetime(csv_data['Date'], format='%Y-%m-%d')
 
 plt.figure(figsize=(16, 9))
-sns.lineplot(y=csv_data['Close'], x=csv_data['Date'])
+sns.lineplot(y=csv_data['Average'], x=csv_data['Date'])
 plt.xlabel('time')
-plt.ylabel('price')
+plt.ylabel('average_price')
+plt.show()
+
+scaler = MinMaxScaler()
+scale_cols = ['High', 'Low', 'Close', 'Average']
+
+csv_scaled = scaler.fit_transform(csv_data[scale_cols])
+
+csv_scaled = pd.DataFrame(csv_scaled)
+csv_scaled.columns = scale_cols
+
+print(csv_scaled)
+
+plt.figure(figsize=(16, 9))
+sns.lineplot(y=csv_scaled['Average'], x=csv_data['Date'])
+plt.xlabel('time')
+plt.ylabel('mean_price')
 plt.show()
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Stock_Prediction
 Stock_Prediction with React.js, Python
+
+
+
++ 2020.09.07  -->  파이썬 csv파일 불러와서 차트 띄우기
++ 2020.09.07  -->  불러온 csv파일 데이터 정규화 [ Data Normalization ]
+

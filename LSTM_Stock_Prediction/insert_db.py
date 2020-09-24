94e8f9e6b986dc93d88eacc110c72000130a0dc1
kim <programmer97@naver.com>
2020-09-24 17:29:57 +0900

    insert_757_db

--- LSTM_Stock_Prediction/insert_db.py
+++ LSTM_Stock_Prediction/insert_db.py
@@ -7,6 +7,7 @@ import pymysql
 import pandas as pd
 import numpy as np
 import pickle
+import glob
 
 HOST = '127.0.0.1'
 PORT = 3306
@@ -22,8 +23,11 @@ conn = pymysql.connect(host=HOST, port=PORT, user=USER, password=PASSWORD, db=DB
 
 cursor = conn.cursor(pymysql.cursors.DictCursor)
 
-now = datetime.datetime.now()
-now_str = now.strftime("%Y-%m-%d")
+# 오늘 날짜 문자열로 치환 #
+# now = datetime.datetime.now()
+# now_str = now.strftime("%Y-%m-%d")
+# ----------------------- #
+
 
 # csv_data = pd.read_csv('C:/dev/react/stock_predict/LSTM_Stock_Prediction/csv/'+now_str+'.csv')
 
@@ -38,6 +42,21 @@ load_pickle = pd.read_pickle('./data.pickle')
 #     cursor.execute(sql)
 # ----------------------------------------- #
 
+def insert_DB(market):
+    file_list = glob.glob('./{}/*.csv'.format(market))
+    for file_name in file_list:
+        file = pd.read_csv(file_name)
+        real_file_name = file_name[8:14]
+        for date, high, low in zip(file['Date'], file['High'], file['Low']):
+            integer_high = int(high)
+            integer_low = int(low)
+            str_high = str(integer_high)
+            str_low = str(integer_low)
+            convert_date = datetime.datetime.strptime(date, "%Y-%m-%d").date()
+            sql = "insert into `{0}` values(%s, %s, %s)".format(real_file_name)
+            cursor.execute(sql, (convert_date, integer_high, integer_low))
+        print("{0}.csv insert completed!!".format(real_file_name))
+
 
 # DB에 csv파일을 이용하여 데이터를 insert #
 # for date, high, low in zip(csv_data['Date'], csv_data['High'], csv_data['Low']):
@@ -46,5 +65,6 @@ load_pickle = pd.read_pickle('./data.pickle')
 #     cursor.execute(sql, (convert_date, high, low))
 # ------------------------------------ #
 
+insert_DB("kospi")
 cursor.close()
 conn.commit()

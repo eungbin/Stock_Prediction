a0d6a9933e3387d7a516202b66213f156328d9ab
kim <programmer97@naver.com>
2020-09-26 18:59:56 +0900

    update-db

--- LSTM_Stock_Prediction/insert_db.py
+++ LSTM_Stock_Prediction/insert_db.py
@@ -3,6 +3,7 @@
 # csv파일을 이용하여 최신 주가정보를 DB에 저장
 
 import datetime
+from pandas_datareader import data
 import pymysql
 import pandas as pd
 import numpy as np
@@ -24,15 +25,20 @@ conn = pymysql.connect(host=HOST, port=PORT, user=USER, password=PASSWORD, db=DB
 cursor = conn.cursor(pymysql.cursors.DictCursor)
 
 # 오늘 날짜 문자열로 치환 #
-# now = datetime.datetime.now()
-# now_str = now.strftime("%Y-%m-%d")
+now = datetime.datetime.now()
+now_str = now.strftime("%Y-%m-%d")
+
+#얘는 어제날짜
+yesterday = now + datetime.timedelta(days=-1)
+str_yesterday = yesterday.strftime("%Y-%m-%d")
+print(str_yesterday)
 # ----------------------- #
 
 
 # csv_data = pd.read_csv('C:/dev/react/stock_predict/LSTM_Stock_Prediction/csv/'+now_str+'.csv')
 
 # pickle 이용하여 data.pickle 파일 load #
-load_pickle = pd.read_pickle('./data.pickle')
+load_pickle = pd.read_pickle('./modify-data.pickle')
 # ----------------------------------- #
 
 # pickle 데이터를 이용하여 반복하여 테이블 생성 #
@@ -42,7 +48,8 @@ load_pickle = pd.read_pickle('./data.pickle')
 #     cursor.execute(sql)
 # ----------------------------------------- #
 
-def insert_DB(market):
+# csv파일을 이용하여 db에 주가정보[약 700여개 기업] 저장 [초기 실행] #
+def init_insert_DB(market):
     file_list = glob.glob('./{}/*.csv'.format(market))
     for file_name in file_list:
         file = pd.read_csv(file_name)
@@ -50,12 +57,31 @@ def insert_DB(market):
         for date, high, low in zip(file['Date'], file['High'], file['Low']):
             integer_high = int(high)
             integer_low = int(low)
-            str_high = str(integer_high)
-            str_low = str(integer_low)
             convert_date = datetime.datetime.strptime(date, "%Y-%m-%d").date()
             sql = "insert into `{0}` values(%s, %s, %s)".format(real_file_name)
             cursor.execute(sql, (convert_date, integer_high, integer_low))
         print("{0}.csv insert completed!!".format(real_file_name))
+# -------------------------------------------------------- #
+count = 0
+def updateDB(stock_code):
+    global count
+    sql = "select date from `{0}` order by date desc limit 1".format(stock_code)
+    cursor.execute(sql)
+    row = cursor.fetchone()
+    if row['date'] >= now.date():
+        print("이미 최신화되어있는 상태입니다.")
+    else:
+        next_row = row['date'] + datetime.timedelta(days=1)
+        str_row_date = str(next_row)
+        update_data = data.get_data_yahoo(stock_code+".ks", str_row_date, now_str)
+        print(update_data.index)
+        print(update_data['High'].values)
+        for date, high, low in zip(update_data.index, update_data['High'], update_data['Low']):
+            convert_date = datetime.datetime.strftime(date, "%Y-%m-%d")
+            sql = "insert into `{0}` values(%s, %s, %s)".format(stock_code)
+            cursor.execute(sql, (convert_date, high, low))
+        print("{0}  |  {1} is complete".format(count, stock_code))
+        count += 1
 
 
 # DB에 csv파일을 이용하여 데이터를 insert #
@@ -65,6 +91,29 @@ def insert_DB(market):
 #     cursor.execute(sql, (convert_date, high, low))
 # ------------------------------------ #
 
-insert_DB("kospi")
+# init_insert_DB("kospi")
+
+# DB 최신화 #
+for code in load_pickle.values:
+    updateDB(code[1])
+# --------- #
+
+# 주식 종목이 존재하는지 확인 [DB이용] #
+# count = 0
+# for code in load_pickle.values:
+#     sql = "select * from `{0}` order by date desc limit 1".format(code[1])
+#     cursor.execute(sql)
+#     row = cursor.fetchone()
+#     print("{0}  |  {1}  |  {2}".format(count, code[1], row))
+#     count += 1
+# --------------------------------- #
+
+# pickle 수정하고 저장 #
+# modify_pickle = load_pickle.drop([load_pickle.index[739]])
+# pd.to_pickle(modify_pickle, './modify-data.pickle')
+# ------------------- #
+
+# for code in modify_pickle.values:
+#     print("{0}  |  {1}".format(code[0], code[1]))
 cursor.close()
 conn.commit()

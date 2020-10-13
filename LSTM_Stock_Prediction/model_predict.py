d5727f7e22ec0d62bb54456a11ecda3a9e88f003
kim <programmer97@naver.com>
2020-10-14 00:40:07 +0900

    planB-python-part-finish

--- LSTM_Stock_Prediction/model_predict.py
+++ LSTM_Stock_Prediction/model_predict.py
@@ -67,9 +67,9 @@ def model_predict(code):
     x_test = np.reshape(x_test, (1, x_test.shape[0], 1))
 
     model = load_model('./models/{0}.h5'.format(code))
-    model.summary()
+    # model.summary()
     pred = model.predict(x_test)
-    print(pred)
+    # print(pred)
     pred_result = (pred+1)*origin
     print("{0}'s result : ".format(code), pred_result)
     return pred_result
@@ -78,38 +78,43 @@ def model_predict(code):
 # 예측 결과를 DB에 저장해주는 함수 #
 def insert_db(dict_result, code_list):
     for code, result in zip(code_list, dict_result):
-        sql = "select exists(select * from `{0}` where date = '2020-09-07')".format(code[1])
+        print("code:{0} || price:{1}".format(code[1], str(dict_result[code[1]])))
+        sql = "select exists(select * from pred_result where code={0})".format(code[1])
         cursor.execute(sql)
         row = cursor.fetchone()
         indexing_sql = sql[7:]  # exists(select * from `005930` where date = '2020-09-07')
-        if(row[indexing_sql] == 1):
+        if(row[indexing_sql] == 1): #만약 DB에 해당 코드의 예측결과가 존재한다면
+            print("{0}의 예측결과가 존재하기 때문에 삭제 후 다시 삽입합니다.".format(code[1]))
             sql = "delete from pred_result where code={0}".format(code[1])
             cursor.execute(sql)
 
         sql = "insert into pred_result values(%s, %s)" #아마 주식코드 + 값
-        cursor.execute(sql, (code[1], dict_result[result]))
+        cursor.execute(sql, (code[1], dict_result[code[1]]))
+        print("{0}'s inserting is complete".format(code[1]))
 
 arr_result = []
 
 # 예측 함수를 실행하여 배열에 결과 저장 #
-# for code in load_pickle.values:
-#     arr_result.append(int(model_predict(code[1])[0][0]))
+for code in load_pickle.values:
+    arr_result.append(int(model_predict(code[1])[0][0]))
 # --------------------------------- #
 print("Predict Result")
 # 예측결과를 종목코드와 쌍으로 딕셔너리 생성 #
-# dict_result = {}
-# for data, code in  zip(arr_result, load_pickle.values):
-#     dict_result[code[1]] = data
+dict_result = {}
+for data, code in  zip(arr_result, load_pickle.values):
+    dict_result[code[1]] = data
+
+print(dict_result)
+
+insert_db(dict_result, load_pickle.values)
+
+# sql = "select exists(select * from `{0}`)".format(code)
+# cursor.execute(sql)
 #
-# for test in dict_result:
-#     print(dict_result[test])
+# row = cursor.fetchone()
+# indexing_sql = sql[7:]      # exists(select * from `005930` where date = '2020-09-07')
+# print("result : " + str(row[indexing_sql]))
+# print(indexing_sql)
 
-sql = "select exists(select * from `{0}` where date = '2020-09-07')".format(code)
-cursor.execute(sql)
-
-row = cursor.fetchone()
-indexing_sql = sql[7:]      # exists(select * from `005930` where date = '2020-09-07')
-print("result : " + str(row[indexing_sql]))
-print(indexing_sql)
-
-conn.close()
+cursor.close()
+conn.commit()
